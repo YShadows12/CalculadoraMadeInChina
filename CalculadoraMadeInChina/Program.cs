fcb27423cb57bbc81ff1ad3282bbeea13867e9f9
YShadows12 <miguel19699@iesmarenostrum.com>
2020-10-19 19:02:32 +0200

    Anti Errores

--- CalculadoraMadeInChina/Program.cs
+++ CalculadoraMadeInChina/Program.cs
@@ -4,14 +4,35 @@ namespace CalculadoraMadeInChina
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void ManageMainMenu()
         {
             while (true)
             {
                 UserInterface.PrintMainMenu();
-                string option = System.Console.ReadLine();
-                System.Console.WriteLine("Has elegido " + option);
+                int option = UserInterface.ReadOption();
+                if (option == 0)
+                {
+                    //break: si esta dentro de un bucle sale de ese bucle, si está dentro de un switch sale del switch
+                    break;
+                }
+                else if (option == -1)
+                {
+                    System.Console.Clear()
+                    System.Console.WriteLine("Opción no válida");
+                }
+                else if (option < 0 || option > 7)
+                {
+                    System.Console.Clear()
+                    System.Console.WriteLine("Opción no válida, por favor escriba un número del 1 al 7");
+                }
             }
+            System.Console.WriteLine("Fin del menú principal");
+        }
+    
+
+        static void Main(string[] args)
+        {
+            ManageMainMenu();
         }
     }
 }
--- CalculadoraMadeInChina/UserInterface.cs
+++ CalculadoraMadeInChina/UserInterface.cs
@@ -8,6 +8,7 @@ namespace CalculadoraMadeInChina
     {
         public static void PrintMainMenu ()
         {
+            System.Console.Clear();
             System.Console.WriteLine("  Calculadora Made in China");
             System.Console.WriteLine("---------------------------");
             System.Console.WriteLine("0) Salir de la aplicacióm");
@@ -25,6 +26,22 @@ namespace CalculadoraMadeInChina
             System.Console.WriteLine("6) Sumario de una serie de números");
             System.Console.WriteLine("---------------------------");
             System.Console.WriteLine("7) Averiguar si tu número es primo");
+        }
+
+        public static int ReadOption()
+        {
+            string option = System.Console.ReadLine();
+            //Cuando algo puede petar se pone esto
+            //Try para lo que queremos hacer, catch lo que saldría si no funcionara, y finally para que pase algo sin importar si ha petado
+            try
+            { 
+                int result = System.Convert.ToInt32(option);
+                return result;
+            }
+            catch (System.Exception e)
+            {
+                return -1;
+            }
         }
     }
 }
